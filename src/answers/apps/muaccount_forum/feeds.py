a896ecd356b85ef24b7de86a2c876fa90529f8cd
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-11-24 12:41:49 +0200

    corrected feed's queryset

--- src/answers/apps/muaccount_forum/feeds.py
+++ src/answers/apps/muaccount_forum/feeds.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext as _
 from django.conf import settings
 
 from forum.feed import RssLastestQuestionsFeed
+from forum.models import Question
 
 class MURssLastestQuestionsFeed(RssLastestQuestionsFeed):
     
@@ -14,3 +15,6 @@ class MURssLastestQuestionsFeed(RssLastestQuestionsFeed):
     def __init__(self, slug, request):
         super(MURssLastestQuestionsFeed, self).__init__(slug, request)
         self.title = request.muaccount.name + _(' - ')+ _('latest questions')
+    
+    def items(self, item):
+        return Question.objects.filter(deleted=False, muaccount=self.request.muaccount).order_by('-added_at')[:30]
